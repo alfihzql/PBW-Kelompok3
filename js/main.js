e78c9736fe2b91218e69fd534452f22e4028b101
N01R <Hizqilhizqil308@gmail.com>
2025-03-24 20:48:14 +0700

    Add files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+function redirectToPage() {
+    const role = document.getElementById("role").value;
+    if(role == "user") {
+        window.location.href = "dasboard_user.html";
+    } else if (role == "admin") {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Silahkan pilih role terlebih dahulu!");
+    }
+}
