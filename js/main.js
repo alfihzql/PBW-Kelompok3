f5574d2c5bb4ea2d8b65153571a64b1f364d31f8
milan <milan23@mhs.usk.ac.id.>
2025-04-20 22:42:58 +0700

    update fetch

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
 
 function redirectToPage() {
-    const role = document.getElementById("role").value;
-    if(role == "user") {
-        window.location.href = "dashboard_user.html";
-    } else if (role == "admin") {
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Silahkan pilih role terlebih dahulu!");
-    }
+  const role = document.getElementById("role").value;
+  if (role == "user") {
+    window.location.href = "dashboard_user.html";
+  } else if (role == "admin") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Silahkan pilih role terlebih dahulu!");
+  }
 }
